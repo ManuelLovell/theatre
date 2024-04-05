31b1a23b497d2e395e3e8e64c1a255bcbb149932
ManuelLovell <69539934+ManuelLovell@users.noreply.github.com>
2024-04-05 01:00:06 +0000

    Build: (f6b8940) UpdateWhatsNews

--- assets/whatsnew-CQutsN6y.js
+++ assets/whatsnew-CQutsN6y.js
@@ -1,4 +1,4 @@
-import{O as o,C as i}from"./bsConstants-Cmf1ZMgY.js";const d=document.querySelector("#bs-whatsnew"),c=document.querySelector("#bs-whatsnew-notes");d.innerHTML=`
+import{O as o,C as i}from"./bsConstants-Cmf1ZMgY.js";const c=document.querySelector("#bs-whatsnew"),d=document.querySelector("#bs-whatsnew-notes");c.innerHTML=`
   <div id="newsContainer">
     <h1>Theatre! 4/4</h1>
     Alright, big Theatre update today.  I've had a lot of requests to allow players to also be able to use this, so I decided to go a step further with changes.
@@ -8,15 +8,16 @@ import{O as o,C as i}from"./bsConstants-Cmf1ZMgY.js";const d=document.querySelec
     </br> - At default, Whisper can be seen/heard by people within two squares. It'll be lower-case with a blue border to hint that it's a whisper.
     </br> - Talk can be seen/heard within 6 squares. It's regular cased with a light-grey border.
     </br> - Yell can be seen/heard within 10 squares. It's upper-cased with a red border.
-    </br>
+    </br> 4. TOKEN OWNERSHIP MATTERS.  As a player, you can only post dialgoue for a token you created or have been given ownership to.
+    </br> 5. The GM can see all conversations.  I feel this is needed, in case something should be reacting to your speech. I'm open to suggestion.
     </br> If there are any hiccups, let me know. I'm letting this one fly early as Theatre could use some love, and I think people will enjoy it.
     </br>
     </div>
-`;o.onReady(async()=>{const s=window.location.search,t=new URLSearchParams(s).get("subscriber")==="true";c.innerHTML=`
+`;o.onReady(async()=>{const s=window.location.search,t=new URLSearchParams(s).get("subscriber")==="true";d.innerHTML=`
     <div id="footButtonContainer">
         <button id="discordButton" type="button" title="Join the Owlbear-Rodeo Discord"><embed class="svg discord" src="/w-discord.svg" /></button>
         <button id="patreonButton" type="button" ${t?'title="Thank you for subscribing!"':'title="Check out the Battle-System Patreon"'}>
         ${t?'<embed id="patreonLogo" class="svg thankyou" src="/thankyou.svg" />':'<embed id="patreonLogo" class="svg patreon" src="/w-patreon.png" />'}</button>
     </div>
     <button id="closeButton" type="button" title="Close this window"><embed class="svg close" src="/w-close.svg" /></button>
-    `;const n=document.getElementById("closeButton");n.onclick=async()=>{await o.modal.close(i.EXTENSIONWHATSNEW)};const r=document.getElementById("discordButton");r.onclick=async e=>{e.preventDefault(),window.open("https://discord.gg/ANZKDmWzr6","_blank")};const a=document.getElementById("patreonButton");a.onclick=async e=>{e.preventDefault(),window.open("https://www.patreon.com/battlesystem","_blank")}});
+    `;const n=document.getElementById("closeButton");n.onclick=async()=>{await o.modal.close(i.EXTENSIONWHATSNEW)};const a=document.getElementById("discordButton");a.onclick=async e=>{e.preventDefault(),window.open("https://discord.gg/ANZKDmWzr6","_blank")};const r=document.getElementById("patreonButton");r.onclick=async e=>{e.preventDefault(),window.open("https://www.patreon.com/battlesystem","_blank")}});
