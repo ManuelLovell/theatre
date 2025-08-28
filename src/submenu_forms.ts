b6cb5559fedc436519c16a1d3bd59a0d376a378c
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2025-08-28 16:44:02 -0700

    oops

--- src/submenu_forms.ts
+++ src/submenu_forms.ts
@@ -3,7 +3,7 @@ export class TheatreForms
     static StoryForm = () =>
         `
         <div class="story-header">
-            <img id="dialog-close"" class="close-icon" src="/close.svg">
+            <img id="dialog-close" class="close-icon" src="/close.svg">
             <img id="dialog-forward" class="forward-icon" src="/play.svg" hidden>
         </div>
         <div class="story-body">
@@ -14,7 +14,7 @@ export class TheatreForms
     static RegularForm = (dialog: IDialog) =>
         `
         <div class="left-column">
-        
+            <img id="dialog-close"" class="close-icon" src="/close.svg">
             <img id="dialog-forward" class="forward-icon" src="/play.svg" hidden>
             <div id="imageHolder"><img src="${dialog.ImageUrl}" onerror="this.src='/error.svg';" alt="Character Image" class="character-image"></div>
         </div>
@@ -31,7 +31,7 @@ export class TheatreForms
     static CompactForm = (dialog: IDialog) =>
         `
         <div class="top-container">
-            <img id="dialog-close"" class="close-icon" src="/close.svg">
+            <img id="dialog-close" class="close-icon" src="/close.svg">
             <img id="dialog-forward" class="mobile-forward-icon" src="/play.svg" hidden>
             <div class="left-top">
                 <div id="imageHolder"><img src="${dialog.ImageUrl}" onerror="this.src='/error.svg';" alt="Character Image" class="character-image"></div>
